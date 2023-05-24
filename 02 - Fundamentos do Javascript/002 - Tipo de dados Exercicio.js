042f758fbb4567c8cf47e74d7fd3f9aa23f299f6
Marcus Castilho <61066188+castpyGH@users.noreply.github.com>
2023-05-24 08:43:33 -0300

    Tipo de dados - Exercício

---
+++ 02 - Fundamentos do Javascript/002 - Tipo de dados Exercicio.js
@@ -0,0 +1,39 @@
+/*
+Exercício 1:
+Declare uma variável chamada idade e atribua a ela
+o valor numérico correspondente à sua idade.
+*/
+let idade = 18
+
+/*
+Exercício 2:
+Declare uma variável chamada 
+nome e atribua a ela o valor correspondente ao seu nome.
+*/
+let nome = "Marcus Castilho"
+
+/* 
+Exercício 3:
+Declare uma variável chamada 
+aprovado e atribua a ela o valor booleano correspondente 
+à sua aprovação em um teste.
+*/
+let aprovado = true
+
+/*
+Exercício 4:
+Declare um array chamado notas e atribua a ele cinco valores 
+numéricos correspondentes às suas notas em diferentes disciplinas.
+*/
+let notas = [10, 7, 8.5, 8, 9.5]
+
+/*
+Exercício 5:
+Declare um objeto chamado pessoa com as propriedades nome, idade e profissao, 
+e atribua os valores correspondentes aos seus dados pessoais.
+*/
+let pessoa = {
+  nome: "Marcus Castilho",
+  idade: 18,
+  profissao: "Programador"
+}
