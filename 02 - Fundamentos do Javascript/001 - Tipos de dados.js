ea204a5b655f41f1e3d455ec978532e7d87fef85
Marcus Castilho <61066188+castpyGH@users.noreply.github.com>
2023-05-24 08:43:07 -0300

    Tipos de dados

---
+++ 02 - Fundamentos do Javascript/001 - Tipos de dados.js
@@ -0,0 +1,28 @@
+//Número (Number)
+let idade = 18
+let altura = 1.75
+
+//Strings
+let nome = "Marcus"
+let mensagem = "Olá, mundo!"
+
+//Booleano (Boolean)
+let aprovado = true
+let reprovado = false
+
+//Array
+let numeros = [1, 2, 3, 4, 5];
+let nomes = ["João", "Maria", "Pedro"];
+
+//Objeto (Object)
+let pessoa = {
+  nome: "Marcus",
+  idade: 18,
+  profissao: "Desenvolvedor Full Stack"
+}
+
+//Null
+let valorNulo = null
+
+//Undefined
+let valorIndefinido = undefined
