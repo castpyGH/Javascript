5392a8b7e2276675bb2c57e7047cfed580c82ec5
Marcus Castilho <61066188+castpyGH@users.noreply.github.com>
2023-05-29 08:44:48 -0300

    Operadores

---
+++ 02 - Fundamentos do Javascript/004 - Operadores.js
@@ -0,0 +1,37 @@
+let a = 10 + 5; // adição
+let b = 10 - 5; // subtração
+let c = 10 * 5; // multiplicação
+let d = 10 / 5; // divisão
+let e = 10 % 5; // módulo (resto da divisão)
+let f = 10 ** 5; // exponenciação
+
+//Operadores de Atribuição
+let x = 10; // atribuição simples
+let y = 5;
+x += y; // atribuição de adição: x = x + y
+x -= y; // atribuição de subtração: x = x - y
+x *= y; // atribuição de multiplicação: x = x * y
+x /= y; // atribuição de divisão: x = x / y
+
+//Operadores de compração
+let a = 5;
+let b = 10;
+let igual = a == b; // igualdade: false
+let diferente = a != b; // diferença: true
+let maior = a > b; // maior que: false
+let menor = a < b; // menor que: true
+let maiorOuIgual = a >= b; // maior ou igual a: false
+let menorOuIgual = a <= b; // menor ou igual a: true
+
+//Operadores lógicos
+let a = true;
+let b = false;
+let resultadoE = a && b; // AND lógico: false
+let resultadoOU = a || b; // OR lógico: true
+let resultadoNao = !a; // NOT lógico: false
+
+//Operadores de concatenação
+let nome = "João";
+let sobrenome = "Silva";
+let nomeCompleto = nome + " " + sobrenome; // concatenação: "João Silva"
+let nomeCompleto02 = `${nome} ${sobrenome}`// concatenação: "João Silva"
